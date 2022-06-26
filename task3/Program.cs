a5fdc380db3b1dea5176903d3252e738fb113edc
Trofimov Anton <Trofimovanton73@gmail.com>
2022-06-26 11:16:57 +0300

    add Programs3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2:
+// Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine($"Максимальное число {numberA} минимальное число {numberB}");   
+}
+else
+{
+    Console.WriteLine($"Максимальное число {numberB} минимальное число {numberA}");
+}
